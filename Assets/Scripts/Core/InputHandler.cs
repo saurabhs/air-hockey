607312e3245dadfe0f6068fe1ec7555199b1eda7
Saurabh S <saurabhs@mailbox.org>
2020-02-27 18:01:21 +0530

    Fix handle input

--- Assets/Scripts/Core/InputHandler.cs
+++ Assets/Scripts/Core/InputHandler.cs
@@ -6,29 +6,35 @@ namespace AirHockey.Core
 {
     public class InputHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPunObservable
     {
-        private Vector3 _start = Vector2.zero;
         private Vector3 _displacement = Vector2.zero;
         [SerializeField] private float _dragMultiplier = 500f;
 
         public Vector3 Displacement => _displacement;
 
-        private float _timeStart = 0;
+        private PhotonView _view = null;
+
+        private void Awake() => _view = GetComponent<PhotonView>();
         
         public void OnBeginDrag(PointerEventData eventData)
         {
-            _timeStart = Time.timeSinceLevelLoad;
+            print("OnBeginDrag");
+            //_timeStart = Time.timeSinceLevelLoad;
         }
 
         public void OnDrag(PointerEventData eventData)
         {
-            _displacement = new Vector3(eventData.delta.x, eventData.delta.y, 0);
-            transform.position += _displacement / _dragMultiplier;
+            if(_view.IsMine)
+            {
+                _displacement = new Vector3(eventData.delta.x, eventData.delta.y, 0);
+                transform.position += _displacement / _dragMultiplier;
+            }
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
             //var time = _displacement / (Time.timeSinceLevelLoad - _timeStart);
         }
+
         public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
         {
             if (stream.IsWriting)
