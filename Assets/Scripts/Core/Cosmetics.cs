d780fe0cbc8f8c019c2b9c913b1b0292ce1f26d4
Saurabh S <saurabhs@mailbox.org>
2020-02-27 18:03:14 +0530

    Move cosmetic set to Awake
    - Add null check to run the main scene without network

--- Assets/Scripts/Core/Cosmetics.cs
+++ Assets/Scripts/Core/Cosmetics.cs
@@ -6,18 +6,16 @@ namespace AirHockey.Core
 {
     public class Cosmetics : MonoBehaviour
     {
-        public TextMeshProUGUI debug = null;
-
         private void Awake()
         {
-            debug = GameObject.FindWithTag("Debug").GetComponent<TextMeshProUGUI>();
-        }
+            var debug = GameObject.FindWithTag("Debug").GetComponent<TextMeshProUGUI>();
 
-        private void Start()
-        {
             var view = GetComponent<PhotonView>();
-            debug.text += $"Cosmetics : Actor No. : {view.Owner.ActorNumber}\n";
-            GetComponent<SpriteRenderer>().color = Constants.GetColor(view.Owner.ActorNumber - 1);
+            if(view != null && view.Owner != null)
+            {
+                debug.text += $"Cosmetics : Actor No. : {view.Owner.ActorNumber}\n";
+                GetComponent<SpriteRenderer>().color = Constants.GetColor(view.Owner.ActorNumber - 1);
+            }
         }
     }
 }
