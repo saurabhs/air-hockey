4f39c1a06ce1e2b3ea28230299e6aa28b8efd472
Saurabh S <saurabhs@mailbox.org>
2020-02-27 15:49:59 +0530

    Add fixed colors to handles across clients

---
+++ Assets/Scripts/Core/Constants.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace AirHockey.Core
+{
+    public class Constants
+    {
+        public static Color GetColor(int index)
+        {
+            switch(index)
+            {
+            case 0:
+                return Color.red;
+            case 1:
+                return Color.green;
+            }
+
+            return Color.yellow;
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Cosmetics.cs
@@ -0,0 +1,23 @@
+﻿using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+namespace AirHockey.Core
+{
+    public class Cosmetics : MonoBehaviour
+    {
+        public TextMeshProUGUI debug = null;
+
+        private void Awake()
+        {
+            debug = GameObject.FindWithTag("Debug").GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Start()
+        {
+            var view = GetComponent<PhotonView>();
+            debug.text += $"Cosmetics : Actor No. : {view.Owner.ActorNumber}\n";
+            GetComponent<SpriteRenderer>().color = Constants.GetColor(view.Owner.ActorNumber - 1);
+        }
+    }
+}
