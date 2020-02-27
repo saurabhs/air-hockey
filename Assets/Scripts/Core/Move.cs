f4c1d6a8f9a8fdca5ad1076d8eed04e3fef86d52
Saurabh S <saurabhs@mailbox.org>
2020-02-27 19:50:33 +0530

    Remove rigibody physics

--- Assets/Scripts/Core/Move.cs
+++ Assets/Scripts/Core/Move.cs
@@ -6,10 +6,35 @@ namespace AirHockey.Core
     [RequireComponent(typeof(Rigidbody2D))]
     public class Move : MonoBehaviour, IPunObservable
     {
-        private Rigidbody2D _rb = null;
-        private void Awake() => _rb = GetComponent<Rigidbody2D>();
+        //private Rigidbody2D _rb = null;
 
-        public void Execute(Vector2 force) => _rb.AddForce(force, ForceMode2D.Force);
+        private bool _canMove = false;
+
+        private Vector2 _force = Vector2.zero;
+
+        private float _speed = 0f;
+
+        private Vector3 _target = Vector3.zero;
+
+        public void Execute(Vector2 force)
+        {
+            _force = force;
+            _speed = _force.magnitude;
+            _canMove = true;
+
+            _target = transform.position + new Vector3(_force.x, _force.y, 0);
+        }
+
+        private void Update()
+        {
+            if(_canMove)
+            {
+                transform.position = Vector2.Lerp(transform.position, _target, _speed * Time.deltaTime);
+
+                if(Vector3.Distance(transform.position, _target) < 0.01f)
+                    _canMove = false;
+            }
+        }
 
         public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
         {
