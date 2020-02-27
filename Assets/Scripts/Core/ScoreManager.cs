e2b489d5dd1528412045be21ec8b22c76505d0ad
Saurabh S <saurabhs@mailbox.org>
2020-02-27 19:51:12 +0530

    Update score Manager to send goals count (doesn't seems to be working though)

--- Assets/Scripts/Core/ScoreManager.cs
+++ Assets/Scripts/Core/ScoreManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace AirHockey.Core
@@ -10,22 +12,47 @@ namespace AirHockey.Core
         public int score;
     }
 
-    public class ScoreManager : MonoBehaviour
+    public class ScoreManager : MonoBehaviour, IPunObservable
     {
         public class OnScoreUpdated : UnityEvent<(int, int)> { }
 
-        [SerializeField] private ScoreData[] scoreData;
+        [SerializeField] private ScoreData[] _scoreData;
 
         public OnScoreUpdated onScoreUpdated = new OnScoreUpdated();
+
+        private PhotonView _view = null;
+
+        private void Awake() => _view = GetComponent<PhotonView>();
+
         private void Start()
         {
-            scoreData[0].goal.onGoalScored.AddListener(OnGoalScored);
-            scoreData[1].goal.onGoalScored.AddListener(OnGoalScored);
+            _scoreData[0].goal.onGoalScored.AddListener(OnGoalScored);
+            _scoreData[1].goal.onGoalScored.AddListener(OnGoalScored);
         }
+
         private void OnGoalScored(int id)
         {
-            scoreData[id].score++;
-            onScoreUpdated.Invoke((scoreData[0].score, scoreData[1].score));
+            _scoreData[id].score++;
+            onScoreUpdated.Invoke((_scoreData[0].score, _scoreData[1].score));
+
+            //_view.RPC("SendScore", RpcTarget.Others);
+        }
+
+        //[PunRPC]
+        //private void SendScore()
+        //{ 
+        //}
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if(stream.IsWriting)
+            {
+                stream.SendNext((object)_scoreData);
+            }
+            else
+            {
+                _scoreData = (ScoreData[])stream.ReceiveNext();
+            }
         }
     }
 }
